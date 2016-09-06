9a559989ee2719052aa2fcb7489b5fabffef5a28
Rahul Alexander <alexander@chroma.org>
2016-09-06 14:35:30 -0400

    added process.env mongodb_URI

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const dbConnection =  (process.env.MONGODB_URI || 'mongodb://localhost:27017/namesake');
+const dbConnection =  process.env['MONGODB_URI'] || 'mongodb://localhost:27017/namesake';
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSalt(10);
 
